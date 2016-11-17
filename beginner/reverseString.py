637c1d90f6ee657b7ba3459a22b2eba6bdbf4fdf
Flavio Castro <2flcastro@gmail.com>
2016-11-17 14:55:10 -0800

    Add unit tests directly into exercise files

--- beginner/reverseString.py
+++ beginner/reverseString.py
@@ -3,7 +3,27 @@
 # Reverse the provided string. Your result must be a string.
 # Try to find alternative methods for the 'slice [::]' notation used on strings.
 # ----------------------------------
+import unittest
 
 def reverseString(str):
     # your code goes here...
     return str
+
+
+
+# ----------------------------------
+# Unit Tests
+class TestReverseString(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(reverseString('hello'), 'olleh')
+
+    def test_2(self):
+        self.assertEqual(reverseString('Howdy'), 'ydwoH')
+
+    def test_3(self):
+        self.assertEqual(reverseString('Greetings from Earth'),
+            'htraE morf sgniteerG')
+
+# Run tests
+if __name__ == '__main__':
+    unittest.main()
