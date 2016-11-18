cd6a64ee160dca4ea14f237f3b7f3ff427a7daf4
Azucena Castro <acastro@berkeley.edu>
2016-11-17 22:59:35 -0800

    Factorial Recursive Solution

--- beginner/factorializeNumber.py
+++ beginner/factorializeNumber.py
@@ -24,7 +24,11 @@ def factorialize(num):
         num -= 1
     return total
 
-
+def recursivefactorial(num):
+    if num == 0 or num == 1:
+        return 1
+    else:
+        return num * recursivefactorial(num -1)
 
 # ----------------------------------
 # Unit Tests
@@ -41,6 +45,15 @@ class TestFactorializeNumber(unittest.TestCase):
     def test_4(self):
         self.assertEqual(factorialize(0), 1)
 
+class TestFactorializeNumber(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(recursivefactorial(5), 120)
+
+    def test_2(self):
+        self.assertEqual(recursivefactorial(20), 2432902008176640000)
+
+    def test_4(self):
+        self.assertEqual(recursivefactorial(0), 1)
 
 # Run Tests
 if __name__ == '__main__':
