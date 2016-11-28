41affdccb93d1659e61d6816b2611b6824a49a20
Flavio Castro <2flcastro@gmail.com>
2016-11-28 01:20:56 -0800

    Solutions for factorializeNumber exercise

--- beginner/factorializeNumber.py
+++ beginner/factorializeNumber.py
@@ -9,19 +9,39 @@
 # Ex: 5! = 5 * 4 * 3 * 2 * 1 = 120
 #
 # Note: you can return whatever variable you want from your
-# function - no necessarily one named "num" - so long as it
+# function - not necessarily one named "num" - so long as it
 # is your solution.
 # -----------------------------------------------------------
 import unittest
 
+# using recursion
 def factorialize(num):
-    # your code here...
-    return num
+    factorial = 1
+    if num == 1 or num == 0:
+        return 1
+    return num * factorialize(num - 1)
+
+# using range()
+def factorialize_2(num):
+    factorial = 1
+    for i in range(num):
+        factorial = factorial * (i + 1);
+    return factorial
+
+# using while loop
+def factorialize_3(num):
+    factorial = 1
+    while num > 0:
+        factorial *= num;
+        num -= 1
+    return factorial
 
 
 
 # ----------------------------------
 # Unit Tests
+# ----------------------------------
+
 class TestFactorializeNumber(unittest.TestCase):
     def test_1(self):
         self.assertEqual(factorialize(5), 120)
@@ -34,6 +54,34 @@ class TestFactorializeNumber(unittest.TestCase):
 
     def test_4(self):
         self.assertEqual(factorialize(0), 1)
+
+
+class TestFactorializeNumber_2(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(factorialize_2(5), 120)
+
+    def test_2(self):
+        self.assertEqual(factorialize_2(10), 3628800)
+
+    def test_3(self):
+        self.assertEqual(factorialize_2(20), 2432902008176640000)
+
+    def test_4(self):
+        self.assertEqual(factorialize_2(0), 1)
+
+
+class TestFactorializeNumber_3(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(factorialize_3(5), 120)
+
+    def test_2(self):
+        self.assertEqual(factorialize_3(10), 3628800)
+
+    def test_3(self):
+        self.assertEqual(factorialize_3(20), 2432902008176640000)
+
+    def test_4(self):
+        self.assertEqual(factorialize_3(0), 1)
 
 
 # Run Tests
