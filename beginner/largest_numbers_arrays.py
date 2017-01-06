b1d7490bbf9850a262abf5fb9c440a597871f85b
Flavio Castro <2flcastro@gmail.com>
2017-01-05 21:08:17 -0800

    Add largest_numbers_arrays exercise

---
+++ beginner/largest_numbers_arrays.py
@@ -0,0 +1,31 @@
+# ----------------------------------
+# Largest Numbers in Arrays
+# ----------------------------------
+# Return an array consisting of the largest number from each provided sub-array.
+# For simplicity, the provided array will contain exactly 4 sub-arrays.
+# ----------------------------------
+import unittest
+
+def largest_of_four(arrs):
+    return largest_arr
+
+
+
+# ----------------------------------
+# Unit Tests
+# ----------------------------------
+class Test_Largest_Of_Four(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(largest_of_four([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39]), [1000, 1001, 857, 1]])
+
+    def test_1(self):
+        self.assertEqual(largest_of_four([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]), [27,5,39,1001])
+
+    def test_1(self):
+        self.assertEqual(largest_of_four([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]), [9, 35, 97, 1000000])
+
+# ----------------------------------
+# Run Tests
+# ----------------------------------
+if __name__ == "__main__":
+    unittest.main()
