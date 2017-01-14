a681cbbb3b1544f893ee6cb29db13652ddb99ebb
Flavio Castro <2flcastro@gmail.com>
2017-01-13 19:18:00 -0800

    Update 2flcastro solutions for largest_numbers_arrays

--- solutions/2flcastro/beginner/largest_numbers_arrays.py
+++ solutions/2flcastro/beginner/largest_numbers_arrays.py
@@ -7,15 +7,21 @@
 
 import unittest
 
-def largest_of_four(arrs):
-    largest_arr = []
-    for arr in arrs:
-        largest_num = arr[0]
-        for number in arr:
+# using list comprehensions
+def largest_of_four(lsts):
+    return [max(lst) for lst in lsts]
+
+
+# looping through each list and its numbers
+def largest_of_four_2(lsts):
+    largest_lst = []
+    for lst in lsts:
+        largest_num = lst[0]
+        for number in lst:
             if number > largest_num:
                 largest_num = number
-        largest_arr.append(largest_num)
-    return largest_arr
+        largest_lst.append(largest_num)
+    return largest_lst
 
 
 
@@ -28,6 +34,14 @@ class Test_Largest_Of_Four(unittest.TestCase):
 
     def test_2(self):
         self.assertEqual(largest_of_four([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]), [9, 35, 97, 1000000])
+
+
+class Test_Largest_Of_Four_2(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(largest_of_four_2([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]), [27,5,39,1001])
+
+    def test_2(self):
+        self.assertEqual(largest_of_four_2([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]), [9, 35, 97, 1000000])
 
 
 # ----------------------------------
