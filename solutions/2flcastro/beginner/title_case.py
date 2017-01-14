80095c76bea51d4f6b52bcb0e9ad3f8bc967ef36
Flavio Castro <2flcastro@gmail.com>
2017-01-13 20:51:28 -0800

    Update solutions for title_case

--- solutions/2flcastro/beginner/title_case.py
+++ solutions/2flcastro/beginner/title_case.py
@@ -10,7 +10,15 @@
 
 import unittest
 
+# using list comprehension and list.capitalize()
 def title_case(sentence):
+    words = sentence.split()
+    words = [word.lower().capitalize() for word in words]
+    return ' '.join(words)
+
+
+# using for-loop and str.capitalize()
+def title_case_2(sentence):
     words = sentence.split()
     for i, word in enumerate(words):
         word = word.lower()
@@ -32,6 +40,17 @@ class Test_Title_Case(unittest.TestCase):
 
     def test_3(self):
         self.assertEqual(title_case("HERE IS MY HANDLE HERE IS MY SPOUT"), "Here Is My Handle Here Is My Spout")
+
+
+class Test_Title_Case_2(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(title_case_2("I'm a little tea pot"), "I'm A Little Tea Pot")
+
+    def test_2(self):
+        self.assertEqual(title_case_2("sHoRt AnD sToUt"), "Short And Stout")
+
+    def test_3(self):
+        self.assertEqual(title_case_2("HERE IS MY HANDLE HERE IS MY SPOUT"), "Here Is My Handle Here Is My Spout")
 
 
 # ----------------------------------
